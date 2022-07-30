3159cad7e9ac6353315c4c04a43dd0452822520d
Jessie Sheng <yjsheng@students.cs.ubc.ca>
2022-07-30 16:39:13 -0700

    Removed comment.

--- src/main/persistence/Writable.java
+++ src/main/persistence/Writable.java
@@ -2,8 +2,6 @@ package persistence;
 
 import org.json.JSONObject;
 
-// based on JsonSerializationDemo: link below
-// https://github.students.cs.ubc.ca/CPSC210/JsonSerializationDemo.git
 public interface Writable {
     // EFFECTS: returns this as JSON object
     JSONObject toJson();
